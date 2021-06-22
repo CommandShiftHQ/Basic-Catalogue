fe6ac7b306ba6d0823235eb64ddb336a22b8c452
StuartCowley <voidstalker@live.co.uk>
2021-06-22 21:16:28 +0100

    fill out ProductList component

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,11 +1,37 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const ProductList = ({ products }) => {
+  const productListWrap = {
+    maxWidth: "600px",
+    marginLeft: "auto",
+    marginRight: "auto",
+  };
+
+  const productListHeader = {
+    display: "flex",
+    justifyContent: "space-evenly",
+    marginBottom: "10px",
+  };
+
+  const productItemWrap = {
+    border: "2px solid black",
+    padding: "10px",
+    marginBottom: "30px",
+  };
+
   return (
-    <div>
+    <div style={productListWrap}>
       {products.map((product) => {
         return (
-          <div>{product.title}</div>
+          <div key={product.id} style={productItemWrap}>
+            <div style={productListHeader}>
+              <div>{product.title}</div>
+              <div>{product.rating}</div>
+            </div>
+            <div>{product.description}</div>
+            <Link to={`product/${product.id}`}>Details</Link>
+          </div>
         );
       })}
     </div>
